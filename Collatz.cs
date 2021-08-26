2d02f2533abcf3a6f2be31263ee0a7b6532eee42
NullBy1e <68380685+NullBy1e@users.noreply.github.com>
2021-08-26 17:33:00 +0200

    Added c# .NET

---
+++ Collatz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _3n_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MainNumber, Parts");
+            int number = Int32.Parse(args[0]);
+            for (int i=1;i<=number;i++){
+                CalculateCollatz(i);
+            }
+        }
+        static void CalculateCollatz(int number){
+            Console.Write(number);
+            Console.Write(",");
+            if (number == 1 ||number ==2){
+                Console.Write("\n");
+            }else{
+                if (number % 2 == 0){
+                    int final_number = number / 2;
+                    CalculateCollatz(final_number);
+                }else{
+                    int final_number = number * 3 + 1;
+                    CalculateCollatz(final_number);
+                }
+            }
+        }
+    }
+}
