dc341a5f6fdebaae9d9283120b1d7813f7320c5c
Piotrek Rybiec <piotrrybiec28@gmail.com>
2021-10-19 20:16:26 +0200

    bash

---
+++ collatz.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+
+collatz () {
+  n=$1
+  echo $n
+  if [[ "$n" -eq 1 ]] || [[ "$n" -eq 2 ]]; then
+    echo -e "\n"
+  else
+    if [[ $(($n % 2)) -eq 0 ]]; then
+      final=$(($n/2))
+      collatz $final
+    else
+      final=$((n*3+1))
+    fi 
+  fi 
+}
+
+numbers=$1
+for (( i=1; i<=$numbers; i++)); do
+  collatz $i 
+done
+  
